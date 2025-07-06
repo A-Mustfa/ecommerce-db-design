614570660cd1ba533839b692909355cb6ae4c38b
Ahmed Mustafa <52112973+A-Mustfa@users.noreply.github.com>
2025-07-06 09:10:06 +0300

    Update 5-products-with-word-camera.sql

--- reporting_queries/5-products-with-word-camera.sql
+++ reporting_queries/5-products-with-word-camera.sql
@@ -1,1 +1,2 @@
+--  Write a SQL query to search for all products with the word "camera" in either the product name or description.
 select * from PRODUCT where product_name like '%camera%' or description like '%camera%';
