2aac1457c23e578229119e4ec0e480f2e46a1d9a
Ahmed Mustafa <52112973+A-Mustfa@users.noreply.github.com>
2025-07-19 09:14:15 +0300

    Delete reporting_queries/5-products-with-word-camera.sql

--- reporting_queries/5-products-with-word-camera.sql
+++
@@ -1,2 +0,0 @@
---  Write a SQL query to search for all products with the word "camera" in either the product name or description.
-select * from PRODUCT where product_name like '%camera%' or description like '%camera%';
