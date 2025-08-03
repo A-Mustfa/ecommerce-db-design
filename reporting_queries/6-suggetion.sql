fb09beb53d724ab3d7a851bad35101f7215df4fa
Ahmed Mustafa <52112973+A-Mustfa@users.noreply.github.com>
2025-08-03 09:08:26 +0300

    Delete reporting_queries directory

--- reporting_queries/6-suggetion.sql
+++
@@ -1,8 +0,0 @@
---  Can you design a query to suggest popular products in the same category for the same author, excluding the Purchsed product from the recommendations?
-
-SELECT P.product_id as [product id], p.product_name as [product name], p.category_id as [category id], p.product_author as [product author]
-from product p join (select p.product_id as 'p_id' , p.category_id as 'c_id' , p.product_author 'p_author' from product p join order_details od
-on p.product_id = od.product_id join orders o on o.order_id = od.order_id
-where o.customer_id = 5) as joinTable on p.product_author = joinTable.p_author
-and p.category_id = joinTable.c_id 
-where p.product_id != joinTable.p_id ;
--- reporting_queries/9-Lock-ROW.sql
+++
@@ -1,6 +0,0 @@
--- transaction query to lock row with id=211 from being updated
-begin transaction
-	select *
-	from PRODUCT with(UPDLOCK, ROWLOCK)
-	where product_id = 211
-
